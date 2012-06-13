9707c74eaa97be8bc451ea37dfa0d9d0532e428a
Maarten Hekkelman <m.hekkelman@cmbi.ru.nl>
2012-06-13 13:50:53 +0000

    met insertie lijst

--- scripts/hssp3to1.pl
+++ scripts/hssp3to1.pl
@@ -24,6 +24,9 @@ die "Max hits exceeds maximum value of 9999\n"
 # Main program, read file and collect data
 
 my (%hits, @hits, @residues, @profile, $nchains, $kchains);
+my ($date, $pdbid, $header, $nchain, $kchain, @query, $seqlen);
+
+$seqlen = $nchain = $kchain = 0;
 
 open(my $h, "<$file") or die "Could not open file $file for reading\n";
 while (my $line = <$h>)
@@ -31,28 +34,169 @@ while (my $line = <$h>)
 	chomp($line);
 	die "Not a valid input file\n" unless ($line eq '# STOCKHOLM 1.0');
 	
-	&readChain($h);
+	$nchain += 1;
+	$header = '';
 	
+	my $chainId;
+	
+	my $state = 1;
+	for (;;)
+	{
+		my $line = <$h>;
+		chomp($line);
+		last if $line eq '//';
+		next if length($line) == 0;
+		
+		if (substr($line, 0, 8) eq '#=GF CC ')
+		{
+			$line = substr($line, 8);
+			
+			if ($line =~ m/^DATE\s+(.+)/) {
+				$date = $1;
+			}
+			elsif ($line =~ m/^PDBID\s+(.+)/) {
+				$pdbid = $1;
+			}
+			elsif ($line =~ m/^(HEADER|COMPND|SOURCE|AUTHOR) (.+)/) {
+				$header = length($header) > 0 ? "$header\n$1     $2" : "$1     $2";
+			}
+		}
+		elsif (substr($line, 0, 8) eq '#=GF RI ')
+		{
+			my %ri;
+			
+			$ri{info} = substr($line, 7, 6) . substr($line, 14, 33) . substr($line, 48, 5) . '  ' . substr($line, 54, 3);
+			$ri{gap} = substr($ri{info}, 12, 1) eq '!' ? 1 : 0;
+
+			push @residues, \%ri;
+		}
+		elsif (substr($line, 0, 8) eq '#=GF PR ')
+		{
+			my $pr = substr($line, 8, 5) . substr($line, 14);
+			push @profile, $pr;
+		}
+		elsif (substr($line, 0, 5) eq '#=GS ')
+		{
+			my ($id, $key, $value) = substr($line, 5) =~ m/(\S+)\s+([A-Z]+) (.+)/;
+			if ($key eq 'CC') {
+				$chainId = $id;
+			}
+			else {
+				unless (defined $hits{$id}) {
+					push @hits, $id;
+					my %ins = ();
+					$hits{$id}{ins} = \%ins;
+					$hits{$id}{jpos} = 1;
+				}
+				$hits{$id}{$key} = $value;
+			}
+		}
+		elsif (substr($line, 0, 1) ne '#')
+		{
+			my ($id, $data) = $line =~ m/^(\S+)\s+(\S+)$/;
+			
+			if ($id eq $chainId)
+			{
+				push @query, split(m//, $data);
+			}
+			else
+			{
+				die "Unexpected line '$line' (id = $id, data = $data)\n" unless defined $hits{$id};
+				push @{$hits{$id}{aln}}, split(m//, $data);
+			}
+		}
+	}
+#	
 #	print Dumper(\%hits);
 }
 close($h);
 
+# 'cut' out the gaps 
 
+my $gap = 0; my $ipos = 1;
+for (my $i = 0; $i < scalar(@query); ++$i)
+{
+	my $r = $query[$i];
+	if ($r ne '.')
+	{
+		++$seqlen;
+
+		if ($gap)
+		{
+			foreach my $hit (values %hits)
+			{
+				next unless defined $hit->{ins}{$gap};
+				
+				my %insertion = (
+					ipos => $ipos,
+					jpos => $hit->{jpos} - length($hit->{ins}{$gap}),
+					iseq => lc(@{$hit->{aln}}[$gap - 1]) . $hit->{ins}{$gap} . lc(@{$hit->{aln}}[$i])
+				);
+				
+				push @{$hit->{insertions}}, \%insertion;
+
+				@{$hit->{aln}}[$gap - 1] = lc @{$hit->{aln}}[$gap - 1];
+				@{$hit->{aln}}[$i] = lc @{$hit->{aln}}[$i];
+			}
+		}
+		
+		$gap = 0;
+		++$ipos;
+	}
+	elsif ($gap == 0)
+	{
+		$gap = $i;
+	}
+	
+	foreach my $hit (values %hits)
+	{
+		if (@{$hit->{aln}}[$i] ne '.')
+		{
+			++$hit->{jpos};
+			if ($gap > 0)
+			{
+				$hit->{ins}{$gap} .= @{$hit->{aln}}[$i];
+			}
+		}
+	}
+}
+
+# clean up hit alignments
+foreach my $hit (values %hits)
+{
+	my $started = 0; my $last = 0;
+	for (my $i = 0; $i < scalar(@{$hit->{aln}}); ++$i)
+	{
+		if (@{$hit->{aln}}[$i] ne '.') {
+			$started = 1;
+			$last = $i;
+		}
+		elsif (not $started) {
+			@{$hit->{aln}}[$i] = ' ';
+		}
+	}
+	
+	for (my $i = $last; $i < scalar(@{$hit->{aln}}); ++$i)
+	{
+		@{$hit->{aln}}[$i] = ' ';
+	}
+}
+
+$seqlen = sprintf("%4d", $seqlen);
+$nchain = sprintf("%4d", $nchain);
+my $nalign = sprintf("%4d", scalar keys %hits);
 
 print<<EOF;
 HSSP       HOMOLOGY DERIVED SECONDARY STRUCTURE OF PROTEINS , VERSION 2.0 2011
-PDBID      $id
+PDBID      $pdbid
 DATE       file generated on $date
 SEQBASE    UniProt
 THRESHOLD  according to: t(L)=(290.15 * L ** -0.562) + 5
 REFERENCE  Sander C., Schneider R. : Database of homology-derived protein structures. Proteins, 9:56-68 (1991).
-CONTACT    Maintained at http://www.cmbi.ru.nl/ by Maarten L. Hekkelman <m.hekkelman@cmbi.ru.nl>
-HEADER     $header
-COMPND     $compnd
-SOURCE     $source
-AUTHOR     $author
+CONTACT    Maintained at http://www.cmbi.ru.nl/ by Maarten L. Hekkelman <m.hekkelman\@cmbi.ru.nl>
+$header
 SEQLENGTH  $seqlen
-NCHAIN     $nchain chain(s) in $id data set
+NCHAIN     $nchain chain(s) in $pdbid data set
 NALIGN     $nalign
 NOTATION : ID: EMBL/SWISSPROT identifier of the aligned (homologous) protein
 NOTATION : STRID: if the 3-D structure of the aligned protein is known, then STRID is the Protein Data Bank identifier as taken
@@ -90,10 +234,14 @@ if (1)
 
 my ($nr, $id, $strid, $ide, $sim, $ifir, $ilas, $jfir, $jlas, $lali, $ngap, $lgap, $lseq2, $acc, $desc);
 #   1 : CRAM_CRAAB          0.98  1.00    2   46    2   46   45    0    0   46  P01542     Crambin;
-format =
+format FMT1 =
 @#### : @<<<<<<<<<<<@<<<   @#.## @#.##@####@####@####@####@####@####@####@####  @<<<<<<<<<<@*
 $nr, $id, $strid, $ide, $sim, $ifir, $ilas, $jfir, $jlas, $lali, $ngap, $lgap, $lseq2, $acc, $desc
 .
+
+	my $ofh = select(STDOUT);
+	$~ = 'FMT1';
+	select($ofh);
 
 	for (my $i = 0; $i < scalar @hits; ++$i)
 	{
@@ -125,57 +273,84 @@ $nr, $id, $strid, $ide, $sim, $ifir, $ilas, $jfir, $jlas, $lali, $ngap, $lgap, $
 	}
 }
 
-sub readChain
-{
-	my ($h) = @_;
-	
-	my ($date, $pdbid, $header);
-	$header = '';
-	
-	my $state = 1;
-	for (;;)
-	{
-		my $line = <$h>;
-		chomp($line);
-		last if $line eq '//';
-		
-		if (substr($line, 0, 8) eq '#=GF CC ')
-		{
-			$line = substr($line, 8);
-			
-			if ($line =~ m/^DATE\s+(.+)/) {
-				$date = $1;
-			}
-			elsif ($line =~ m/^PDBID\s+(.+)/) {
-				$pdbid = $1;
-			}
-			elsif ($line =~ m/^(HEADER|COMPND|SOURCE|AUTHOR) (.+)/) {
-				$header .= $line . "\n";
-			}
-		}
-		elsif (substr($line, 0, 8) eq '#=GF RI ')
-		{
-			my %ri;
-			
-			$ri{'info'} = substr($line, 7, 6) . substr($line, 14, 33) . substr($line, 48, 5) . '  ' . substr($line, 54, 3);
-			
-			push @residues, \%ri;
-		}
-		elsif (substr($line, 0, 8) eq '#=GF PR ')
-		{
-			my $pr = substr($line, 8, 5) . substr($line, 14);
-			push @profile, $pr;
-		}
-		elsif (substr($line, 0, 5) eq '#=GS ')
-		{
-			my ($id, $key, $value) = substr($line, 5) =~ m/(\S+)\s+([A-Z]+) (.+)/;
-			next if $key eq 'CC';
+# print alignments
 
-			push @hits, $id unless defined $hits{$id};
-			$hits{$id}{$key} = $value;
+my $N = scalar(keys %hits);
+
+for (my $i = 0; $i < $N; $i += 70)
+{
+	my $last = $i + 70;
+	$last = $N unless $last <= $N;
+	
+	printf("## ALIGNMENTS %4d - %4d\n SeqNo  PDBNo AA STRUCTURE BP1 BP2  ACC NOCC  VAR  ", $i + 1, $last);
+	for (my $a = $i / 10; $a < $i / 10 + 7; ++$a) {
+		printf("....:....%d", ($a + 1) % 10);
+	}
+	print "\n";
+
+	my $resnr = 0;
+	foreach my $res (@residues)
+	{
+		my $aln = '';
+		if (not $res->{gap})
+		{
+			$resnr += 1 while $query[$resnr] eq '.';
+			
+			for (my $j = $i; $j < $last; ++$j)
+			{
+				$aln .= @{$hits{$hits[$j]}->{'aln'}}[$resnr];
+			}
+			++$resnr;
 		}
+		
+		print $res->{info}, "  $aln\n";
 	}
 }
+
+# print profile
+
+print<<EOF;
+## SEQUENCE PROFILE AND ENTROPY
+ SeqNo PDBNo   V   L   I   M   F   W   Y   G   A   P   S   T   C   H   R   K   Q   E   N   D  NOCC NDEL NINS ENTROPY RELENT WEIGHT
+EOF
+
+print join("\n", @profile), "\n";
+
+print<<EOF;
+## INSERTION LIST
+ AliNo  IPOS  JPOS   Len Sequence
+EOF
+
+for (my $nr = 1; $nr <= scalar(@hits); ++$nr)
+{
+	my $hit = $hits{$hits[$nr - 1]};
+
+	next unless defined $hit->{insertions};
+
+	my ($ipos, $jpos, $len, $iseq);
+
+format FMT2 = 
+@##### @#### @#### @#### ^<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+$nr,   $ipos,$jpos,$len, $iseq
+~~                       ^<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+                         $iseq
+.
+	my $ofh = select(STDOUT);
+	$~ = 'FMT2';
+	select($ofh);
+
+	foreach my $ins (@{$hit->{insertions}})
+	{
+		$ipos = $ins->{ipos};
+		$jpos = $ins->{jpos};
+		$iseq = $ins->{iseq};
+		$len = length($iseq) - 2;
+
+		write;
+	}
+}
+
+print "//\n";
 
 __END__
 
